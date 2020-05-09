30930a96df454129e4beddc3e10247b7e977f736
Shawn Silverman <shawn@pobox.com>
2020-05-09 15:21:00 -0700

    Add basic support for contentEncoding and contentMediaType annotations

---
+++ src/main/java/com/qindesign/json/schema/keywords/ContentMediaType.java
@@ -0,0 +1,33 @@
+/*
+ * Created by shawn on 5/9/20 11:29 AM.
+ */
+package com.qindesign.json.schema.keywords;
+
+import com.google.gson.JsonElement;
+import com.qindesign.json.schema.Keyword;
+import com.qindesign.json.schema.MalformedSchemaException;
+import com.qindesign.json.schema.Validator;
+import com.qindesign.json.schema.ValidatorContext;
+
+/**
+ * Implements the "contentMediaType" annotation.
+ */
+public class ContentMediaType extends Keyword {
+  public static final String NAME = "contentMediaType";
+
+  public ContentMediaType() {
+    super(NAME);
+  }
+
+  @Override
+  protected boolean apply(JsonElement value, JsonElement instance, ValidatorContext context)
+      throws MalformedSchemaException {
+    if (!Validator.isString(value)) {
+      context.schemaError("not a string");
+      return false;
+    }
+
+    context.addAnnotation(NAME, value.getAsString());
+    return true;
+  }
+}
