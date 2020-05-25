fe8b7470bc8a0dcbef55b31d76fbc06133f3420d
Shawn Silverman <shawn@pobox.com>
2020-05-24 21:19:18 -0700

    Add JSON tree traversal
    This is in preparation for the new linter. Also updating the Javadocs for JSON.

--- src/main/java/com/qindesign/json/schema/JSON.java
+++ src/main/java/com/qindesign/json/schema/JSON.java
@@ -22,7 +22,12 @@ import java.io.Reader;
 import java.nio.charset.StandardCharsets;
 
 /**
- * Provides JSON tools.
+ * Provides JSON tools, including:
+ * <ol>
+ * <li>Parsing</li>
+ * <li>Element checking</li>
+ * <li>Tree traversal</li>
+ * </ol>
  */
 public final class JSON {
   private JSON() {
@@ -118,5 +123,91 @@ public final class JSON {
    */
   public static boolean isString(JsonElement e) {
     return e.isJsonPrimitive() && e.getAsJsonPrimitive().isString();
+  }
+
+  /**
+   * Visitor for JSON tree traversal.
+   */
+  public interface JsonElementVisitor {
+    /**
+     * Visits a JSON element. The {@code state} argument holds more information
+     * about the element and its position in the tree. It holds information that
+     * won't necessarily change for every element.
+     *
+     * @param e the element being visited
+     * @param parent the element's parent
+     * @param path the full path to the element, a JSON Pointer
+     * @param state holds more information about the element
+     */
+    void visit(JsonElement e, JsonElement parent, String path, TraverseState state);
+  }
+
+  /**
+   * Holds state during a JSON tree traversal. It holds information that won't
+   * necessarily change for every element.
+   */
+  public static final class TraverseState {
+    /** The current specification, as determined by the latest $schema value. */
+    public Specification spec;
+
+    TraverseState copy() {
+      TraverseState copy = new TraverseState();
+      copy.spec = this.spec;
+      return copy;
+    }
+  }
+
+  /**
+   * Traverses a JSON tree and visits each element using {@code visitor}. This
+   * uses a preorder ordering.
+   *
+   * @param e the root of the JSON tree
+   * @param visitor the visitor
+   */
+  public static void traverse(JsonElement e, JsonElementVisitor visitor) {
+    traverse(e, null, "", new TraverseState(), visitor);
+  }
+
+  /**
+   * Recursive method that performs the traversal.
+   *
+   * @param e the current element
+   * @param parent the element's parent
+   * @param path the element's full path, a JSON Pointer
+   * @param state the tree state
+   * @param visitor the visitor
+   */
+  private static void traverse(JsonElement e, JsonElement parent, String path,
+                               TraverseState state,
+                               JsonElementVisitor visitor) {
+    // Possibly alter the state
+    Specification spec = Validator.specificationFromSchema(e);
+    if (spec != null) {
+      state = state.copy();
+      state.spec = spec;
+    }
+
+    visitor.visit(e, parent, path, state);
+
+    if (e.isJsonPrimitive() || e.isJsonNull()) {
+      return;
+    }
+
+    if (e.isJsonArray()) {
+      int index = 0;
+      for (var item : e.getAsJsonArray()) {
+        traverse(item, e, path + "/" + index, state, visitor);
+        index++;
+      }
+      return;
+    }
+
+    // Process everything else
+    for (var entry : e.getAsJsonObject().entrySet()) {
+      String name = entry.getKey();
+      name = name.replace("~", "~0");
+      name = name.replace("/", "~1");
+      traverse(entry.getValue(), e, path + "/" + name, state, visitor);
+    }
   }
 }
