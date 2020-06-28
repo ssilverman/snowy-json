85371f9f9d360f8cbb5a0a590e150315f07c0cab
Shawn Silverman <shawn@pobox.com>
2020-06-27 20:20:50 -0700

    Add missing licence comments to JSONPath and Coverage

--- src/main/java/com/qindesign/json/schema/JSONPath.java
+++ src/main/java/com/qindesign/json/schema/JSONPath.java
@@ -1,4 +1,22 @@
 /*
+ * Snow, a JSON Schema validator
+ * Copyright (c) 2020  Shawn Silverman
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/*
  * Created by shawn on 6/23/20 6:19 PM.
  */
 package com.qindesign.json.schema;
